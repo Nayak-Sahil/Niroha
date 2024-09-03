37eba5337145cec2775a4e52c299230fa93fd2b0
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-09-03 11:57:13 +0530

    Fix: UI Bugs

--- src/ExplorePlants/ExplorePlants.jsx
+++ src/ExplorePlants/ExplorePlants.jsx
@@ -3,14 +3,13 @@ import SearchPlants from "./SearchPlants";
 import PlantCard from "./PlantCard";
 
 {
-  /* <h1 className='text-xl font-medium text-center text-primary leading-10'>Explore the Healing Power of Plants and Herbs.</h1> */
-}
-{
-  /* <p className='font-medium text-primary-foreground text-center'>Search plants by name and instantly access detailed information on their healing benefits.</p> */
+  /* Search plants by name and instantly access detailed information on their healing benefits. */
 }
 export default function ExplorePlants() {
+// bg-top filter invert
+
   return (
-    <section className="w-screen p-5 px-10 h-[90vh] flex flex-col justify-evenly items-center bg-[url('https://www.3daistudio.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBackground.86dd7208.png&w=3840&q=75')]">
+    <section className="w-screen p-5 px-10 h-[90vh] flex flex-col justify-evenly items-center">
       <SearchPlants />
       <div className="w-full h-[85%] gap-y-14 overflow-y-scroll grid grid-cols-4 justify-items-center">
         <PlantCard />
--- src/ExplorePlants/SearchPlants.jsx
+++ src/ExplorePlants/SearchPlants.jsx
@@ -16,13 +16,13 @@ import React from "react";
 export default function SearchPlants() {
   return (
     <fieldset className="w-[95%] h-[20%] flex justify-center items-center p-4 glassmorphism rounded-lg mb-5 border-none">
-      <legend className="-ml-1 px-3 text-base tracking-wider font-medium text-white">
+      <legend className="-ml-1 px-3 text-base tracking-wide font-medium text-white">
         Explore the Healing Power of Plants and Herbs
       </legend>
       <div className="w-full h-[20%] flex items-center justify-between">
         <div className="w-[400px] flex items-center relative">
           <Input
-            className="w-full mr-5 glassmorphism rounded-full capitalize text-base py-3 text-white px-4 shadow-lg outline-none focus-visible:ring-offset-0 focus-visible:ring-0 border-none"
+            className="w-full mr-5 glassmorphism rounded-full capitalize text-sm py-3 text-white px-4 shadow-lg outline-none focus-visible:ring-offset-0 focus-visible:ring-0 border-none"
             placeholder="Search your herbal plant by name!"
           />
           <Button variant="secondary" className="btn-gradient text-black px-5 absolute right-1 rounded-full">
