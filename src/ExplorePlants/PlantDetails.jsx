785d8a68129a42d942d657be5ccf56d7ead321a8
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-09-03 16:11:27 +0530

    Update: PlantDetails Component

--- src/ExplorePlants/PlantDetails.jsx
+++ src/ExplorePlants/PlantDetails.jsx
@@ -7,7 +7,7 @@ import PlantBenefits from "./PlantBenefits";
 
 export default function PlantDetails() {
   return (
-    <div className="w-screen p-5 px-10 h-[90vh] flex justify-evenly items-center">
+    <div className="relative w-screen p-5 px-10 h-[90vh] flex justify-evenly items-center">
       <PlantBasic />
       <section className="h-full flex flex-col justify-evenly">
         <PlantBenefits />
@@ -15,6 +15,11 @@ export default function PlantDetails() {
         <PlantVisuals />
       </section>
       <PlantMedicinal />
+      {/* <iframe
+        src="https://aichatbot.sendbird.com/playground/index.html?app_id=1638BC62-01FD-4402-A03B-06C5BF67CC95&bot_id=onboarding_bot&region=ca-1"
+        className="absolute bottom-15 right-5 w-[400px] h-[500px]"
+        frameborder="0"
+      ></iframe> */}
     </div>
   );
 }
