cd3afc08797041cfb9d2b3f1a3597ad7e0734853
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-09-03 16:10:11 +0530

    Update: PlantVisual Component

--- src/ExplorePlants/PlantVisuals.jsx
+++ src/ExplorePlants/PlantVisuals.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import img from "../assets/plantsImage/ginger.png";
 import { Button } from "@/components/ui/button";
-import { Box, Images, MoonStar, Sprout, TvMinimalPlay, Video } from "lucide-react";
+import {
+  Box,
+  Images,
+  MoonStar,
+  Sprout,
+  TvMinimalPlay,
+  Video,
+} from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import {
   Tooltip,
@@ -9,6 +16,18 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import AloveraModel from "@/ModelView/AloveraModel";
+import ModelViewer from "@/Landing/ModelViewer";
+import LandingModel from "@/Landing/LandingModel";
+import ModelCanvas from "@/Landing/ModelCanvas";
 
 export default function PlantVisuals() {
   return (
@@ -34,7 +53,8 @@ export default function PlantVisuals() {
           icon={<MoonStar className="h-4 w-4" />}
         />
       </div>
-      <img className="m-auto" src={img} alt="Plants Image" width={300} />
+      <VisualCarousel />
+      {/* <img className="m-auto" src={img} alt="Plants Image" width={300} /> */}
       <Badge
         className="absolute -bottom-3 left-[50%] -translate-x-[50%] w-max mx-auto px-2 py-1"
         variant="secondary"
@@ -45,6 +65,40 @@ export default function PlantVisuals() {
     </section>
   );
 }
+
+export const PlantImages = () => {
+  return <img className="m-auto" src={img} alt="Plants Image" width={300} />;
+};
+
+export const PlantVideos = () => {
+  return (
+    <video
+      controls
+      className="w-[220px] h-[300px] mx-auto rounded-lg shadow-lg object-cover"
+    >
+      <source src="/public/Tulsi2.mp4" />
+    </video>
+  );
+};
+
+export const VisualCarousel = () => {
+  return (
+    <Carousel className="w-[300px] mb-2 max-w-xs mx-auto ">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              {/* <PlantVideos /> */}
+              <PlantImages />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="glassmorphism  ml-8" />
+      <CarouselNext className="glassmorphism  mr-8" />
+    </Carousel>
+  );
+};
 
 export const VisualModeButton = ({ icon, isDayNightMode, hoverText }) => {
   return (
@@ -60,7 +114,7 @@ export const VisualModeButton = ({ icon, isDayNightMode, hoverText }) => {
             {icon}
           </Badge>
         </TooltipTrigger>
-        <TooltipContent className="rounded-full font-medium text-[12px] glassmorphism text-white shadow-xl mb-5 relative -left-2">
+        <TooltipContent className="rounded-full font-medium text-[12px] glassmorphism text-white shadow-xl relative -left-2">
           <p>{hoverText}</p>
         </TooltipContent>
       </Tooltip>
